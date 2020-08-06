c2322e80df337220a730bf3ad9840a5a809a0ce1
GokoshiJr <julioagonzalez18@gmail.com>
2020-08-06 12:37:36 -0400

    Update functions

---
+++ definidaPositiva.m
@@ -0,0 +1,10 @@
+function definidaPositiva(Matriz)
+  contador = 0;
+  
+  if (Matriz(1,1) > 0)
+    contador++;  
+  endif
+  
+  
+  
+endfunction
---
+++ matrizSimetrica.m
@@ -0,0 +1,9 @@
+function simetria(A)  
+   
+  if (A == A')
+    printf 'Es simetrica la matriz \n';
+  else
+    printf 'No es simetrica la matriz \n';
+    
+  endif
+endfunction
