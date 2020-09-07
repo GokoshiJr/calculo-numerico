9fdf88269833505e51cee6fd2f049e8688e06910
GokoshiJr <julioagonzalez18@gmail.com>
2020-09-06 21:18:41 -0400

    Start e3 Newton-Raphson

---
+++ src/evaluacion3/cargarEcuacion.m
@@ -0,0 +1,45 @@
+function r = cargarEcuacion()
+  
+  linea  = ' -----------------------------------------';
+  clc;
+  
+  disp(linea);
+  fprintf('%29s \n','Cargar Ecuacion');
+  disp(linea);
+  
+  fprintf('   1) Ecuacion --> e^x = N / X \n   2) Otra ecuacion\n') ;
+  disp(linea);
+  fprintf('   Opcion: ');
+  opc = input('');
+  disp(linea);
+  
+  if (opc == 1) 
+    
+    fprintf('  Indique el valor de N: ');
+    n = input('');
+    fprintf('  Indique el valor del punto inicial x0: ');
+    xi = input('');
+    fprintf('  Indique la tolerancia: ');
+    tol = input('');
+    disp(linea);    
+    fprintf('   Se ha cargado correctamente\n');
+    disp(linea);
+    esperar = input('   Presione Enter para continuar');
+    
+  elseif (opc == 2)
+    fprintf('   Indique la ecuacion');
+    esperar = input('   Presione Enter para continuar');
+  else
+    fprintf('   Ingrese una opcion valida por favor');
+  endif
+  
+  
+  
+  
+#  ecuacion.symbolic = strcat('(x*e^x)/', mat2str(n));
+#  ecuacion.N = n;
+#  ecuacion.xi = xi;
+  
+  # return
+#  r = ecuacion;
+endfunction
---
+++ src/evaluacion3/main.m
@@ -0,0 +1,44 @@
+linea  = ' -------------------------------------------';
+clc;
+salir = false;
+
+do
+  disp(linea);
+  disp("  Evaluacion 3 - Metodo de Newton-Raphson");
+  disp(linea);
+  disp("  1) Cargar ecuacion");
+  disp("  2) Iteraciones");
+  disp("  3) Interpolar Punto");
+  disp("  4) Grafica");
+  disp("  5) Salir");
+  disp(linea);
+  opc = input("  Seleccione una opcion: ");  
+  
+  switch (opc)
+    
+    case 1        
+      disp('caso 1')
+      ecuacion = cargarEcuacion();
+      ecuacion
+    case 2       
+      disp('caso 2')
+      
+    case 3      
+      disp('caso 3')
+      
+    case 4
+      disp('caso 4')
+      
+    case 5  
+      disp(linea);
+      disp("  Fin del Programa");
+      salir = true;
+      
+    otherwise
+      clc;
+      disp('');
+      disp("  Ingrese una opcion valida\n");
+      
+  endswitch
+  
+until(salir == true);
